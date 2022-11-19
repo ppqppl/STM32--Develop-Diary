dcc45bd0aad3e979e7adcb9307d2e1fdfb123f76
ppqppl <1683127550@qq.com>
2022-11-20 02:07:09 +0800

    STM32

---
+++ stm32/OLED_CubeMX/OLED_HAL/gui.h
@@ -0,0 +1,69 @@
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//测试硬件：单片机STM32F103RCT6,正点原子MiniSTM32开发板,主频72MHZ，晶振12MHZ
+//QDtech-OLED液晶驱动 for STM32
+//xiao冯@ShenZhen QDtech co.,LTD
+//公司网站:www.qdtft.com
+//淘宝网站：http://qdtech.taobao.com
+//wiki技术网站：http://www.lcdwiki.com
+//我司提供技术支持，任何技术问题欢迎随时交流学习
+//固话(传真) :+86 0755-23594567 
+//手机:15989313508（冯工） 
+//邮箱:lcdwiki01@gmail.com    support@lcdwiki.com    goodtft@163.com
+//技术支持QQ:3002773612  3002778157
+//技术交流QQ群:324828016
+//创建日期:2018/8/27
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 深圳市全动电子技术有限公司 2018-2028
+//All rights reserved
+/****************************************************************************************************
+//=========================================电源接线================================================//
+// OLED模块               STM32单片机
+//   VCC         接       DC 5V/3.3V      //OLED屏电源正
+//   GND         接          GND          //OLED屏电源地
+//=======================================液晶屏数据线接线==========================================//
+//本模块默认数据总线类型为4线制SPI
+// OLED模块               STM32单片机
+//   D1          接          PB15        //OLED屏SPI写信号
+//=======================================液晶屏控制线接线==========================================//
+// OLED模块               STM32单片机
+//   CS          接          PB11        //OLED屏片选控制信号
+//   RES         接          PB12        //OLED屏复位控制信号
+//   DC          接          PB10        //OLED屏数据/命令选择控制信号
+//   D0          接          PB13        //OLED屏SPI时钟信号
+//=========================================触摸屏接线=========================================//
+//本模块不带触摸功能，所以不需要触摸屏接线
+****************************************************************************************************/	
+/***************************************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, QD electronic SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+****************************************************************************************************/		
+#ifndef __GUI_H__
+#define __GUI_H__
+
+void GUI_DrawPoint(u8 x, u8 y, u8 color);
+void GUI_Fill(u8 sx,u8 sy,u8 ex,u8 ey,u8 color);
+void GUI_DrawLine(u8 x1, u8 y1, u8 x2, u8 y2,u8 color);
+void GUI_DrawRectangle(u8 x1, u8 y1, u8 x2, u8 y2,u8 color);
+void GUI_FillRectangle(u8 x1, u8 y1, u8 x2, u8 y2,u8 color);
+void GUI_DrawCircle(u8 xc, u8 yc, u8 color, u8 r);
+void GUI_FillCircle(u8 xc, u8 yc, u8 color, u8 r);
+void GUI_DrawTriangel(u8 x0,u8 y0,u8 x1,u8 y1,u8 x2,u8 y2,u8 color);
+void GUI_FillTriangel(u8 x0,u8 y0,u8 x1,u8 y1,u8 x2,u8 y2,u8 color);
+void GUI_ShowChar(u8 x,u8 y,u8 chr,u8 Char_Size,u8 mode);
+void GUI_ShowNum(u8 x,u8 y,u32 num,u8 len,u8 Size,u8 mode);
+void GUI_ShowString(u8 x,u8 y,u8 *chr,u8 Char_Size,u8 mode);
+void GUI_ShowFont16(u8 x,u8 y,u8 *s,u8 mode);
+void GUI_ShowFont24(u8 x,u8 y,u8 *s,u8 mode);
+void GUI_ShowFont32(u8 x,u8 y,u8 *s,u8 mode);
+void GUI_ShowCHinese(u8 x,u8 y,u8 hsize,u8 *str,u8 mode);
+void GUI_DrawBMP(u8 x,u8 y,u8 width, u8 height, u8 BMP[], u8 mode); 
+#endif
+
